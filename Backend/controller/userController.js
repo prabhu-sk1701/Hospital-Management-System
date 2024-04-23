0316300b65d948d921ca5a8720104d171b520950
prabhu-sk1701 <prabhu.sk1701@gmail.com>
2024-04-23 16:04:01 +0530

    User login

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -47,3 +47,29 @@ export const patientRegister = catchAsyncErrors(async(req,res,next)=>{
         message: "User Successfully Registered."
     });
 });
+
+
+export const login = catchAsyncErrors(async(req,res,next)=>{
+    const {email, password, confirmPassword, role} = req.body;
+    if(!email || !password || !confirmPassword || !role){
+        return next(new ErrorHandler("Please provide all details", 400));
+    }
+    if(password !== confirmPassword){
+        return next(new ErrorHandler("Passwords do no match! Please try again.", 400));
+    }
+    const user = await User.findOne({email}).select("+password");
+    if(!user){
+        return next(new ErrorHandler("Incorrect email or Password.", 400));
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Incorrect email or Password.", 400));
+    }
+    if(role !== user.role){
+        return next(new ErrorHandler("User with this role not found.", 400));
+    }
+    res.status(200).json({
+        success: true,
+        message: "User Successfully Logged in."
+    });
+});
