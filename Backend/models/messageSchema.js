b91ce7b4bd212b824c02f988fcc8c8201779d647
prabhu-sk1701 <prabhu.sk1701@gmail.com>
2024-04-24 19:53:38 +0530

    Homepage complete

--- Backend/models/messageSchema.js
+++ Backend/models/messageSchema.js
@@ -20,13 +20,13 @@ const messageSchema = new mongoose.Schema({
     phone:{
         type: String,
         required: true,
-        minLength: [11, "Phone number must contain exactly 11 digits"],
-        maxLength: [11, "Phone number must contain exactly 11 digits"], 
+        minLength: [10, "Phone number must contain exactly 10 digits"],
+        maxLength: [10, "Phone number must contain exactly 10 digits"], 
     },
     message:{
         type: String,
         required: true,
-        minLength: [11, "Message must contain at least 10 characters"],        
+        minLength: [10, "Message must contain at least 10 characters"],        
     },
 });
 
--- Frontend/src/components/MessageForm.jsx
+++ Frontend/src/components/MessageForm.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const MessageForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+  const handleMessage = async (e) => {
+    e.preventDefault();
+    try {
+      await axios
+        .post(
+          "http://localhost:4000/api/v1/message/send",
+          { firstName, lastName, phone, email, message },
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          toast.success(res.data.message);
+          setFirstName("");
+          setLastName("");
+          setPhone("");
+          setEmail("");
+          setMessage("");
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
   return (
-    <div>MessageForm</div>
-  )
-}
+    <div className="container form-component message-form">
+      <h2>Send us a Message</h2>
+      <form onSubmit={handleMessage}>
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Email id"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Phone number"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
+        <textarea
+          rows={10}
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <div style={{ justifyContent: "center", alignItems: "center" }}>
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default MessageForm
+export default MessageForm;
